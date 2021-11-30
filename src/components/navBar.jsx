4d0ddd5e5bb0ab8ecee5dba056a67f520d3b0ca5
Mnabawy <m_ninja61@yahoo.com>
2021-11-30 11:37:06 +0200

    build

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
@@ -22,16 +22,7 @@ const NavBar = () => {
                 Movies
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/rentals">
-                Rentals
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/customers">
-                Customers
-              </NavLink>
-            </li>
+
             <li className="nav-item">
               <NavLink className="nav-link" to="/login">
                 Login
@@ -46,7 +37,7 @@ const NavBar = () => {
         </div>
       </nav>
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
